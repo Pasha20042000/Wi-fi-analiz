3606e7458512068c965e79d070d69b96e435dabd
Pasha20042000 <pavel.chernov.0020@bk.ru>
2022-02-21 17:17:58 +0300

    First commit

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 from libhackrf import *
+import numpy as np
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.fftpack
+
 
 
 hackrf = HackRF()
@@ -8,11 +13,22 @@ hackrf.center_freq = 88.5e6
 
 sn = hackrf.get_serial_no()
 print(sn)
-samples = hackrf.read_samples()
+while(True):
+    samples = hackrf.read_samples()
 
-
-crypt
-
+    # количество семплов
+    N = samples.size
+    # 1/sample_rate
+    T = 1.0 /hackrf.sample_rate
+    x = np.linspace(0.0, N*T, N)
+    #samples
+    y = samples
+    yf = scipy.fftpack.fft(y)
+    xf = np.linspace(0.0, 1.0/(2.0*T), N//2)
+    print(samples[:100])
+    # fig, ax = plt.subplots()
+    # ax.plot(xf, 2.0/N * np.abs(yf[:N//2]))
+    # plt.show()
 
 
 #result = libhackrf.hackrf_set_sample_rate(dev, 20e6)
